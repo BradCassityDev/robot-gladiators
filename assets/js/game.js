958048bad98f079973a9ee4bc6dfe5bef6a2efeb
Brad Cassity <bradley.cassity@gmail.com>
2020-08-12 21:21:38 -0600

    Wrapping up the MVP with start and end Functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,16 +11,10 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
 // Enemy variables
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-/*console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames[3]);
-console.log(enemyNames.length);*/
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -78,26 +72,46 @@ var enemyAttack = 12;
         }
         
     }
-
-    
-    
 }
 
 
+var startGame = function() {
+    // Set Player Stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
-for(var i = 0; i < enemyNames.length; i++) {
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+    
+            // Run fight function with new enemy
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    endGame();
+    //startGame();
+}
 
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-
-        // Run fight function with new enemy
-        fight(pickedEnemyName);
+var endGame = function() {
+    if(playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        window.alert("You've lost your robot in battle");
     }
 
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if(playAgainConfirm) {
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
 }
+
+startGame();
